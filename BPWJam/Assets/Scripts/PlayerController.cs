9757634b7f62aeb74c7ec2c667e9345a9b7074fe
Rutger van Teutem <>
2021-03-15 15:28:48 +0100

    Added player controller

---
+++ BPWJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    public float moveSpeed = 20f;
+    public bool isGrounded = false;
+    public GameObject deathScreen;
+    float jumpTimer = 0;
+    public int coins = 0;
+    Rigidbody2D rb;
+    //public GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(!gm.isPaused){
+        if (Input.GetKey(KeyCode.W))
+            Jump();
+    }
+
+    void Jump()
+    {
+        if (isGrounded && jumpTimer <= 0)
+        {
+            jumpTimer = .2f;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        jumpTimer -= Time.deltaTime;
+    }
+
+    void Die()
+    {
+        //Player Dies
+        //gm.isPaused = true;
+        PlayerPrefs.SetString("Coins", coins.ToString());
+        deathScreen.SetActive(true);
+        Debug.Log("Dead!");
+    }
+
+
+    //Collisions
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "DeathBarrier")
+        {
+            Die();
+        }
+        if(collision.tag == "Coin")
+        {
+            coins += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+}
